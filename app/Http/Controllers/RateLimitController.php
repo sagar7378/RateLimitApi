86bf9a7ea0645dc0954ddd9cc5741db2c232e960
sagar7378 <sagarprajapat250@gmaiil.com>
2024-02-21 20:34:08 +0530

    upload rate limit api code

---
+++ app/Http/Controllers/RateLimitController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RateLimitController extends Controller
+{
+    public function index(Request $request)
+    {
+        $data = [
+            "id" => 1,
+            "title" => "iPhone 9",
+            "description" => "An apple mobile which is nothing like apple",
+            "price" => 549,
+            "discountPercentage" => 12.96,
+            "rating" => 4.69,
+            "stock" => 94,
+            "brand" => "Apple",
+            "category" => "smartphones",
+            "thumbnail" => "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg",
+            "images" => [
+                "https://cdn.dummyjson.com/product-images/1/1.jpg",
+                "https://cdn.dummyjson.com/product-images/1/2.jpg",
+                "https://cdn.dummyjson.com/product-images/1/3.jpg",
+                "https://cdn.dummyjson.com/product-images/1/4.jpg",
+                "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"
+            ]
+        ];
+
+        return response()->json($data);
+    }
+
+}
